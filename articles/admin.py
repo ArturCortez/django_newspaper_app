9e7cf20cae5aa5c53746cf9808355baddccef000
ArturCortez <arturcortez.ms@gmail.com>
2024-01-15 19:55:21 +0000

    added rudimentary comments

--- articles/admin.py
+++ articles/admin.py
@@ -1,6 +1,18 @@
 # articles/admin.py
 
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment 
 
-admin.site.register(Article)
+class CommentInLine(admin.TabularInline): 
+    model = Comment
+    extra = 0 # new
+
+class ArticleAdmin(admin.ModelAdmin):
+    
+    inlines = [ 
+        CommentInLine,
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment) 
